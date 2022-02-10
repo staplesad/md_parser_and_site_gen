0dc55a8bcda45d576daf41551075c8f3c48fa787
staplesad <sadhbhsdoyle@gmail.com>
2022-02-10 23:36:35 +0000

    add more tests

--- test/Test.hs
+++ test/Test.hs
@@ -7,12 +7,18 @@ import Text.Megaparsec
 
 import Md.Parser
 
-getParseOutput parser input = runParser parser "" input
+testParser :: (Eq a, Show a) => Parser a -> String -> a -> Assertion
+testParser parser input expected = runParser parser "" input @?= Right expected
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Parser"
-  [ testCase "Raw Text" $ getParseOutput normalText "This is a 1. piece of normal ` text ~!" @?= (Right $ Raw "This is a 1. piece of normal ` text ~!")
+tests = testGroup "El Parsers"
+  [ testCase "Raw Text" $ testParser normalText "This is a 1. piece of normal ` text ~!" (Raw "This is a 1. piece of normal ` text ~!")
+  , testCase "Strong Text" $ testParser strongText "__A strong piece of text__" (Strong [ Raw "A strong piece of text"])
+  , testCase "Emph Text" $ testParser emphText "_A emph piece of text_" (Emphasis [Raw "A emph piece of text"])
+  , testCase "Nested emph" $ testParser emphText "_A emph __and strong__ piece of text_" (Emphasis [Raw "A emph ", Strong [Raw "and strong"], Raw " piece of text"])
+  , testCase "Nested strong" $ testParser strongText "___strong and emph___" (Strong [Emphasis [ Raw "strong and emph"]])
+  , testCase "Strict Text" $ testParser strictText "This is a 1. piece of normal ` text ~!" (Raw "This is a 1. piece of normal ` text ~!")
   ]
