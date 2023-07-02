6cf4633e386e9fcc3aa800c2890b33e10178873b
staplesad <sadhbhsdoyle@gmail.com>
2023-07-02 19:15:58 +0200

    in progress

--- lib/Md/Parser.hs
+++ lib/Md/Parser.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Md.Parser where
 
+import Debug.Trace
 import Control.Monad
 import Data.Void
 import Data.Char
@@ -24,30 +24,115 @@ data Mchars = Line String | LineEnding
 instance Eq Mchars where
   Line s1 == Line s2 = s1 == s2
   LineEnding == LineEnding = True
+  LineEnding == Line _ = False
+  Line _ == LineEnding = False
 
-data Block = BList [Block] | OList [Block] | Title Int [Mchars] | Break | CodeBlock [Mchars] | Paragraph [Mchars] | BlankLine
+data Block
+  = BList [(Block, Int)]
+  | OList [(Block, Int)]
+  | Title Int Mchars
+  | Break
+  | CodeBlock String
+  | Paragraph [Mchars]
+  | BlankLine
   deriving (Show, Eq)
 
 myEOL = void (chunk "\r\n" <|> chunk "\n" <|> chunk "\r") <|> eof
 
 lineEndParser :: Parser Mchars
 lineEndParser = LineEnding <$ myEOL
+-- fmap . const
 -- fmap (\_ -> LineEnding) (chunk ....)
+
+singleLineChar :: Char -> Bool
+singleLineChar = (`notElem` ("\r\n" :: String))
 
 lineParser :: Parser Mchars
 lineParser = do
-  hspace
-  a <- textFirstChar
-  b <- takeWhileP (Just "Bare String") (`notElem` ("\r\n" :: String))
+  a <- try $ do
+    hspace
+    textFirstChar
+  b <- takeWhileP (Just "Bare String") singleLineChar
   lineEndParser
   pure $ Line (a:b)
 
 titleParser :: Parser Block
 titleParser = do
-  --pure (Title 0 [LineEnding])
+  title_depth <- try $ do
+    hspace
+    some (char '#')
+  let n = length title_depth
+  hspace
+  title_text <- takeWhile1P (Just "Title String") singleLineChar
+  lineEndParser
+  pure $ Title n (Line title_text)
+
+breakParser :: Parser Block
+breakParser = do
+  try $ do
+    hspace
+    count 3 $ oneOf ("-*~")
+  some $ oneOf ("-*~")
+  hspace
+  lineEndParser
+  pure Break :: Parser Block
+
+
+indentStringParser :: Int -> Parser String
+indentStringParser n = do
+  s <- takeWhile1P (Just "Codeblock String") (/= '`')
+  s' <- try ("" <$ count n (char '`')) <|> continue
+  pure $  s <> s'
+  where
+    continue :: Parser String
+    continue = do
+      char '`'
+      s' <- indentStringParser n
+      pure $ '`' : s'
+
+codeBlockParser :: Parser Block
+codeBlockParser = do
+  try $ do
+    hspace
+    count 3 $ char '`'
+  extra_backticks <- many $ char '`'
+  let n = length extra_backticks
+  s <- indentStringParser (n+3)
+  many (char '`')
+  hspace
+  lineEndParser
+  pure $ CodeBlock s
+
+bListItemParser :: Parser (Block, Int)
+bListItemParser = do
+  indents <- many $ char ' '
+  let n = length indents
+  oneOf "-*+"
+  b <- bBlockOption
+  pure (b, n)
+
+bListParser :: Parser Block
+bListParser = try $ do
+  b <- some bListItemParser
+  pure $ BList b
+
+oListItemParser :: Parser (Block, Int)
+oListItemParser = do
+  indents <- many $ char ' '
+  let n = length indents
+  satisfy isDigit
+  char '.'
+  char ' '
+  o <- oBlockOption
+  pure (o, n)
+
+oListParser :: Parser Block
+oListParser = try $ do
+  o <- some oListItemParser
+  pure $ OList o
 
 textFirstChar :: Parser Char
-textFirstChar = satisfy (`notElem` ("*-#`0123456789" :: String))
+textFirstChar = satisfy (`notElem` ("+*-#`0123456789" :: String))
 
 paragraphParser :: Parser Block
 paragraphParser = Paragraph <$> some lineParser
@@ -56,12 +141,37 @@ paragraphParser = Paragraph <$> some lineParser
 --(<$) :: Functor f => b -> f a -> f b
 
 blankParser :: Parser Block
-blankParser = BlankLine <$ (hspace >> lineEndParser)
+blankParser = try $ BlankLine <$ (hspace >> lineEndParser)
 -- hspace uses takeWhileP and consumes tokens so this will not backtrack.
 -- blankParser should be the last topLevel block tried for this reason
 
+oBlockOption :: Parser Block
+oBlockOption
+  = oListParser
+  <|> titleParser
+  <|> codeBlockParser
+  <|> breakParser
+  <|> paragraphParser
+  <|> blankParser
+
+bBlockOption :: Parser Block
+bBlockOption
+  = bListParser
+  <|> titleParser
+  <|> codeBlockParser
+  <|> breakParser
+  <|> paragraphParser
+  <|> blankParser
+
 blockOption :: Parser Block
-blockOption = titleParser <|> blankParser
+blockOption
+  = bListParser
+  <|> oListParser
+  <|> titleParser
+  <|> codeBlockParser
+  <|> breakParser
+  <|> paragraphParser
+  <|> blankParser
 
 document :: Parser Document
 document = many blockOption <* eof
--- test/Test.hs
+++ test/Test.hs
@@ -10,7 +10,9 @@ import Text.Megaparsec
 import Md.Parser
 
 testParser :: (Eq a, Show a) => Parser a -> String -> a -> Assertion
-testParser parser input expected = runParser parser "" input @?= Right expected
+testParser parser input expected = case runParser parser "" input of
+  Left err -> fail $ errorBundlePretty err
+  Right a -> a @?= expected
 
 testFail :: Parser a -> String -> Assertion
 testFail parser input = isLeft (runParser parser "" input) @?= True
@@ -25,9 +27,43 @@ tests = testGroup "Parse Blocks"
     , testCase "TextLine" $ testParser paragraphParser "some text\n" (Paragraph [Line "some text"])
     , testCase "failTextLine" $ testFail paragraphParser " * sometxt \n"
     , testCase "multiTextLine" $ testParser paragraphParser "some text\n       another text\n" (Paragraph [Line "some text", Line "another text"])
-    -- ]
-    -- , testGroup "Component Parsers"
-    -- [ testCase "textFirstChar" $ testParser textFirstChar "o" 'o'
-    -- , testCase "failTextFirstChar" $ testFail textFirstChar "*"
+    , testCase "multiTextLine2" $ testParser paragraphParser "some text\r\n       another text\r\n" (Paragraph [Line "some text", Line "another text"])
+    , testCase "simpleBreak" $ testParser breakParser " ----- \n" Break
+    , testCase "complicatedBreak" $ testParser breakParser " ~-*---~~~~----* \n" Break
+    , testCase "failBreakSpace" $ testFail breakParser " ------- ~~~~~"
+    , testCase "failBreakMin" $ testFail breakParser "~~\n"
+    ],
+    testGroup "Parent Blocks"
+    [
+      testCase "title1" $ testParser titleParser " # A Title" (Title 1 (Line "A Title"))
+    , testCase "title2" $ testParser titleParser "     ##     title words \n" (Title 2 (Line "title words "))
+    , testCase "titleSpace" $ testParser titleParser " # #Title\r" (Title 1 (Line "#Title"))
+    , testCase "failTitleBlank" $ testFail titleParser "###      \n"
+    , testCase "laaaaaargeTitle" $ testParser titleParser "###### 6" (Title 6 (Line "6"))
+    , testCase "basicCodeBlock" $ testParser codeBlockParser "``` Here's some code\n````" (CodeBlock  " Here's some code\n")
+    , testCase "backtickCodeBlock" $ testParser codeBlockParser "``` a ` here```" (CodeBlock " a ` here")
+    , testCase "noEOLCodeBlock" $ testFail codeBlockParser "```sf``` some more"
+    , testCase "moreOpeningCodeBlock" $ testParser codeBlockParser "````lol`````" (CodeBlock "lol")
+    , testCase "failMoreOpeningCodeBlock" $ testFail codeBlockParser "````lol```"
+    ],
+    testGroup "RecursiveBlocks"
+    [
+      testCase "basicList" $ testParser bListParser "- some text\n-line 2\n" (BList [(Paragraph [Line "some text"], 0), (Paragraph[Line "line 2"], 0)])
+    , testCase "indentedList" $ testParser bListParser "  - first\n" (BList [(Paragraph [Line "first"], 2)])
+    , testCase "indented2List" $ testParser bListParser "  - first\n  - second\n" (BList [(Paragraph [Line "first"], 2), (Paragraph [Line "second"], 2)])
+    , testCase "nestedList" $ testParser bListParser "- first\n  - indent\n" (BList [(Paragraph [Line "first"], 0), (Paragraph [Line "indent"], 2)])
+    , testCase "codeBlockList" $ testParser bListParser "- ```abcdef   l```\n- other" (BList [(CodeBlock "abcdef   l", 0), (Paragraph [Line "other"], 0)])
+    , testCase "titleBlockList" $ testParser bListParser "- # title\n- other" (BList [(Title 1 (Line "title"), 0), (Paragraph [Line "other"], 0)])
+    , testCase "listBlockList" $ testParser bListParser "-  - words\n     - words" (BList [(BList [(Paragraph [Line "words"], 2), (Paragraph [Line "words"], 5)], 0)])
+    , testCase "basicOList" $ testParser oListParser "5. words\n1. more words" (OList [(Paragraph [Line "words"], 0), (Paragraph [Line "more words"], 0)])
+    , testCase "indentedOList" $ testParser oListParser "1. w\n  2. x\n1. w" (OList [(Paragraph [Line "w"], 0), (Paragraph [Line "x"], 2), (Paragraph [Line "w"], 0)])
+    , testCase "mixLists" $ testFail oListParser "1. something\n  - x\n2.  s"
+    , testCase "mixListsBlocks" $ testParser (some blockOption) "1. something\n  - x\n2.  s" [OList [(Paragraph [Line "something"], 0)], BList [(Paragraph [Line "x"], 2)], OList [(Paragraph [Line "s"], 0)]]
+    , testCase "emptyItem" $ testParser bListParser "- x\n-  \n- y" (BList [(Paragraph [Line "x"], 0), (BlankLine, 0), (Paragraph [Line "y"], 0)])
+    ]
+    , testGroup "Component Parsers"
+    [ testCase "textFirstChar" $ testParser textFirstChar "o" 'o'
+    , testCase "failTextFirstChar" $ testFail textFirstChar "*"
+    , testCase "indentString" $ testParser (indentStringParser 3) "    \n     dkalmfkasldmf \n ##  \n```" "    \n     dkalmfkasldmf \n ##  \n"
     ]
  ]
